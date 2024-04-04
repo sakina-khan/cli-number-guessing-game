79877ac75418b2258b84ba410eba25542b51a92c
sakina-khan <164930378+sakina-khan@users.noreply.github.com>
2024-04-04 11:16:52 -0700

    Add files via upload
    number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWellcome to CodeWithSakina - CLT Number Guessing Game\n");
+const randomeNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessNumber === randomeNumber) {
+    console.log("Congratulations! You Guess a correct number");
+}
+else {
+    console.log("Sorry, you guess a wrong number");
+}
